83a7f0b1a3be2b9a4bfb523541c5ce2f16224566
yorktronic <ty.walker@gmail.com>
2016-08-03 18:39:06 -0700

    started twitter data pull script

---
+++ get-twitter-data.py
@@ -0,0 +1,8 @@
+import twitter
+consumer_key = "fZovH0fFnbSMb3EKKJazxIIkr"
+consumer_secret = "M73H7prpgoDz3Zh2RGg6BMal97Ajc9C0KvOxGI7VVm1TDbG938"
+access_token_key = "407343684-oxBp1vDzdkVC7YvEFHZYbS4fgUw8QpS2HvMcpu2E"
+access_token_secret = "Gh6K1iexwvQMVTo52TJmG7mSbAWzf0TtdWd4blSNOyzuz"
+
+api = twitter.Api(consumer_key=consumer_key, consumer_secret=consumer_secret,
+	access_token_key=access_token_key, access_token_secret=access_token_secret)
