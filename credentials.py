8d921e7985941f6dc915d3d1a915ad329b33d22c
yorktronic <ty.walker@gmail.com>
2016-08-22 17:44:59 -0700

    adding credential file because its conveniennt

---
+++ credentials.py
@@ -0,0 +1,5 @@
+# Twitter API credentials
+twitter_consumer_key = "WtpuFmhTdigCR06g6debqf9un"
+twitter_consumer_secret = "kp98LeDTenKnsDk81A2YpHh3unplHyWgSro1bYb4Rr4D9oxx9i"
+twitter_access_key = "407343684-oxBp1vDzdkVC7YvEFHZYbS4fgUw8QpS2HvMcpu2E"
+twitter_access_secret = "Gh6K1iexwvQMVTo52TJmG7mSbAWzf0TtdWd4blSNOyzuz"
